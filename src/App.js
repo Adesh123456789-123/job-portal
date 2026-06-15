c089fbd4f318038288c3cb2e17a1c0764933bac4
Adesh123456789-123 <9675sjk@gmail.com>
2026-06-15 15:55:50 +0530

    Day 2 - Add search functionality

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
+import { useState } from "react";
 import Navbar from "./components/Navbar";
 import JobCard from "./components/JobCard";
+import SearchBar from "./components/SearchBar";
 import jobs from "./data/jobs";
 import "./App.css";
 
 function App() {
+  const [search, setSearch] = useState("");
+
+  const filteredJobs = jobs.filter((job) =>
+    job.title.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
     <div>
       <Navbar />
 
+      <div style={{ padding: "20px" }}>
+        <SearchBar
+          search={search}
+          setSearch={setSearch}
+        />
+      </div>
+
       <div className="job-container">
-        {jobs.map((job) => (
+        {filteredJobs.map((job) => (
           <JobCard
             key={job.id}
             job={job}
